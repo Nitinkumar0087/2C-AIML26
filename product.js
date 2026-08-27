ec500c5a5d99319631c16a9882680b1706fa3c8b
Nitin kumar <nitinkumar70174@gmail.com>
2026-08-27 15:42:30 +0530

    Add files via upload

---
+++ product.js
@@ -0,0 +1,30 @@
+let product = {
+    p_id : 101,
+    p_name : "shirt",
+    p_price : 200,
+    p_quantity : 3,
+
+    cost : function(){
+        return this.p_price * this.p_quantity;
+    },
+    update_quantity:function(new_quantity){
+        this.p_quantity = new_quantity;
+    },
+    display:function(){
+        console.log("Product id:" + this.p_id);
+        console.log("Product name:" + this.p_name);
+        console.log("product price:" + this.p_price);
+        console.log("Product quantity:") + this.p_quantity;
+        console.log("cost:" + product.cost());
+
+    
+    }
+};
+//calling cost function
+product.cost();
+//calling update quantity
+product.update_quantity(5);
+//calling cost function after updating quantity
+product.cost()
+//display funtion
+product.display();
